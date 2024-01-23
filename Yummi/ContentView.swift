bc128693c902df4860f5b18b7fbda1da9c5d6df5
Koza Kurumlu <kozakurumlu@gmail.com>
2024-01-23 10:01:47 +0000

    Initial Commit

---
+++ Yummi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yummi
+//
+//  Created by Kurumlu, Koza (RCH) on 23/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Kurumlu, Koza (RCH) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
